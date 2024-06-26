fcb3fa3dc7c9ee3427b5e029c267b8a6eef19e13
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-06-26 12:30:44 +0900

    [Bronze V] Title: N 찍기, Time: 1028 ms, Memory: 30336 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+public class Main {
+	public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int N = Integer.parseInt(br.readLine());
+        
+        for(int i = 1; i <= N ; i++){
+            System.out.println(i);
+        }
+	}
+}
