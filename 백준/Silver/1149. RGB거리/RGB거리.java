ada287370a34b60b63b2d29ef0c6483759271fea
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-08-30 19:18:42 +0900

    [Silver I] Title: RGB거리, Time: 116 ms, Memory: 14656 KB -BaekjoonHub

---
+++ 백준/Silver/1149. RGB거리/RGB거리.java
@@ -0,0 +1,31 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int N = Integer.parseInt(br.readLine());
+        
+        int[][] cost = new int[N][3];
+        for (int i = 0; i < N; i++) {
+            StringTokenizer st = new StringTokenizer(br.readLine());
+            for (int j = 0; j < 3; j++) {
+                cost[i][j] = Integer.parseInt(st.nextToken());
+            }
+        }
+        
+        int[][] dp = new int[N][3];
+        dp[0][0] = cost[0][0];
+        dp[0][1] = cost[0][1];
+        dp[0][2] = cost[0][2];
+        
+        for (int i = 1; i < N; i++) {
+            dp[i][0] = Math.min(dp[i-1][1], dp[i-1][2]) + cost[i][0];
+            dp[i][1] = Math.min(dp[i-1][0], dp[i-1][2]) + cost[i][1];
+            dp[i][2] = Math.min(dp[i-1][0], dp[i-1][1]) + cost[i][2];
+        }
+        
+        int result = Math.min(Math.min(dp[N-1][0], dp[N-1][1]), dp[N-1][2]);
+        System.out.println(result);
+    }
+}
