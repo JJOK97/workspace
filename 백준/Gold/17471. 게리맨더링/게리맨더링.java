94c90c67183e3100d0b770cda2a73b8d797a13c4
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-09-08 17:40:08 +0900

    [Gold III] Title: 게리맨더링, Time: 116 ms, Memory: 16300 KB -BaekjoonHub

--- 백준/Gold/17471. 게리맨더링/게리맨더링.java
+++ 백준/Gold/17471. 게리맨더링/게리맨더링.java
@@ -1,48 +1,65 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-    static int n, min;
-    static int[] people;
-    static boolean[] check;
-    static int[] arr; // 인덱스:지역 , 요소값:선거구
-    static ArrayList<ArrayList<Integer>> graph = new ArrayList<ArrayList<Integer>>();// 간선표시
 
-    public static void main(String[] args) throws IOException {
+    static int n, min;
+    static int[] people, arr;
+    static List<List<Integer>> graph = new ArrayList<>();
+
+    public static void main(String[] args) throws Exception{
+
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
         n = Integer.parseInt(br.readLine());
         min = Integer.MAX_VALUE;
         people = new int[n];
         arr = new int[n + 1];
 
         StringTokenizer st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < n; i++)
+
+        for(int i = 0; i < n; i++){
             people[i] = Integer.parseInt(st.nextToken());
+        }
 
-        for (int i = 0; i <= n; i++)
+        for(int i = 0; i <= n; i++){
             graph.add(new ArrayList<Integer>());
+        }
 
-        for (int i = 1; i <= n; i++) {
+        for(int i = 1; i <= n; i++){
             st = new StringTokenizer(br.readLine());
             int cnt = Integer.parseInt(st.nextToken());
-            for (int j = 0; j < cnt; j++)
+            for(int j = 0; j < cnt; j++){
                 graph.get(i).add(Integer.parseInt(st.nextToken()));
+            }
         }
-        /////////////////////////////////////////////////////////////////// 입력
 
-        // 선거구 조합
         back(1);
-        if (min == Integer.MAX_VALUE)
+        if(min == Integer.MAX_VALUE){
             System.out.println(-1);
-        else
+        } else {
             System.out.println(min);
+        }
+
     }
+
+    static boolean[] check;
 
     public static void back(int depth){
 
         if(depth == n + 1){
 
             check = new boolean[n + 1];
+
+            int cnt = 0;
+
+            for(int i = 1; i <= n; i++){
+                if(!check[i]){
+                    link(i , arr[i]);
+                    cnt++;
+                }
+
+            }
 
             int a = 0, b = 0;
 
@@ -54,22 +71,11 @@ public class Main {
                 }
             }
 
-            int cnt = 0;
-
-            for(int i = 1; i <= n; i++){
-                if(check[i]){
-                    continue;
-                }
-                link(i , arr[i]);
-                cnt++;
-            }
-            
             if(cnt == 2){
                 min = Math.min(min, Math.abs(a - b));
             }
-            
-            return;
 
+            return;
         }
 
         arr[depth] = 1;
@@ -77,27 +83,26 @@ public class Main {
 
         arr[depth] = 0;
         back(depth + 1);
-
     }
-    
-    public static void link(int num, int local){
-        Queue<Integer> q = new LinkedList<Integer>();
-        q.add(num);
-        check[num] = true;
-        
+
+    public static void link(int depth, int local){
+        Queue<Integer> q = new LinkedList<>();
+        q.add(depth);
+        check[depth] = true;
+
         while(!q.isEmpty()){
             int cur = q.poll();
-            
+
             for(int next : graph.get(cur)){
-                
+
                 if(arr[next] == local && !check[next]){
                     check[next] = true;
                     q.add(next);
                 }
-                
+
             }
         }
-        
+
     }
-    
+
 }
