96558363f6bad4a9ae434945f5552d8428960ca6
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-09-11 22:01:17 +0900

    [Gold V] Title: 로봇 청소기, Time: 104 ms, Memory: 14388 KB -BaekjoonHub

--- 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
+++ 백준/Gold/14503. 로봇 청소기/로봇 청소기.java
@@ -1,123 +1,105 @@
-import java.io.*;
 import java.util.*;
+import java.io.*;
 
 public class Main {
-
-    static int[] dX = {-1, 0, 1, 0};
-    static int[] dY = {0, 1, 0, -1};
-
+    static int[] dr = {-1, 0, 1, 0}; // 북, 동, 남, 서
+    static int[] dc = {0, 1, 0, -1}; // 북, 동, 남, 서
+    static int N, M, cr, cc, dir, cnt;
     static int[][] map;
-    static int[][] check;
+    static boolean[][] visited;
 
-    static int N, M, r, c, dir;
-
-    static int result = 0;
-
-    public static void main(String[] args) throws Exception{
-
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         N = Integer.parseInt(st.nextToken());
         M = Integer.parseInt(st.nextToken());
 
+        map = new int[N][M];
+        visited = new boolean[N][M];
+
         st = new StringTokenizer(br.readLine());
 
-        r = Integer.parseInt(st.nextToken());
-        c = Integer.parseInt(st.nextToken());
+        cr = Integer.parseInt(st.nextToken());
+        cc = Integer.parseInt(st.nextToken());
         dir = Integer.parseInt(st.nextToken());
 
-        map = new int[N][M];
-        check = new int[N][M];
-
-        for(int i = 0; i < N; i++){
+        for(int i = 0; i < N; i++) {
             st = new StringTokenizer(br.readLine());
-            for(int j = 0; j < M; j++){
-                map[i][j] = Integer.parseInt(st.nextToken());
-                if( map[i][j] == 1 ){
-                    check[i][j] = 1;
+            for(int j = 0; j < M; j++) {
+                int tmp = Integer.parseInt(st.nextToken());
+                map[i][j] = tmp;
+                if(tmp == 1) visited[i][j] = true;
+            }
+        }
+
+        cnt = 0;
+
+        while(true) {
+            // 1. 현재 칸이 더러울 경우
+            if(isDirty(cr,cc)) {
+                cnt++;
+            }
+
+            // 2. 주변 4칸 모두 깨끗할 경우
+            if(isAllClean(cr, cc)) {
+                // 2-1. 후진할 수 있다면 후진하고 1번으로 돌아감
+                if(canGoBack(cr, cc)) {
+                    continue;
+                    // 2-2. 뒤쪽이 벽이라면 작동 멈춤
+                } else {
+                    break;
                 }
             }
-        }
-
-        firstCase();
-
-        System.out.println(result);
-
-    }
-
-    public static void firstCase(){
-
-        if(check[r][c] == 0) {
-            result++;
-            check[r][c] = 2;
-        }
-
-        boolean checkMove = false;
-
-        for(int i = 0; i < 4; i++){
-            int nX = r + dX[i];
-            int nY = c + dY[i];
-
-            if(checkMap(nX, nY) && map[nX][nY] == 0 && check[nX][nY] == 0){
-                checkMove = true;
-                break;
+            // 3. 주변이 더러울 경우
+            else {
+                // 3-1. 반시계 방향 회전
+                dir = (dir + 3) % 4;
+                int nr = cr + dr[dir];
+                int nc = cc + dc[dir];
+                // 3-2. 앞 칸이 청소되지 않은 빈 칸일 경우 전진
+                if(dirCheck(nr, nc) && map[nr][nc] == 0 && !visited[nr][nc]) {
+                    cr = nr;
+                    cc = nc;
+                }
+                // 3-3. 1번으로 돌아감
             }
         }
-
-        if(!checkMove){
-
-            if(secondCase()){
-                return;
-            }
-
-        } else {
-
-            thirdCase();
-
-        }
+        System.out.println(cnt);
     }
 
-    public static boolean secondCase(){
-
-        int backDir = (dir + 2) % 4;
-        int nX = r + dX[backDir];
-        int nY = c + dY[backDir];
-
-        if(checkMap(nX, nY) && map[nX][nY] == 0){
-            r = nX;
-            c = nY;
-            firstCase();
-        } else {
+    static boolean isDirty(int r, int c) {
+        if(map[r][c] == 0 && !visited[r][c]) {
+            visited[r][c] = true;
             return true;
         }
-
         return false;
-
     }
 
-    public static void thirdCase(){
+    static boolean isAllClean(int r, int c) {
+        for(int d = 0; d < 4; d++) {
+            int nr = r + dr[d];
+            int nc = c + dc[d];
 
-        for(int i = 0; i < 4; i++){
-            dir = (dir + 3) % 4;
-            int nX = r + dX[dir];
-            int nY = c + dY[dir];
-
-            if(checkMap(nX, nY) && map[nX][nY] == 0 && check[nX][nY] == 0){
-                r = nX;
-                c = nY;
-                firstCase();
-                return;
+            if(dirCheck(nr, nc) && map[nr][nc] == 0 && !visited[nr][nc]) {
+                return false;
             }
         }
-
-        firstCase();
-
+        return true;
     }
 
-    public static boolean checkMap(int nX, int nY){
-        return 0 <= nX && nX < N && 0 <= nY && nY < M;
+    static boolean canGoBack(int r, int c) {
+        int backDir = (dir + 2) % 4;
+        int nr = r + dr[backDir];
+        int nc = c + dc[backDir];
+        if(dirCheck(nr, nc) && map[nr][nc] == 0) {
+            cr = nr;
+            cc = nc;
+            return true;
+        }
+        return false;
     }
 
+    public static boolean dirCheck(int nX, int nY){
+        return 0 <= nX && 0 <= nY && nX < N && nY < M;
+    }
 }
