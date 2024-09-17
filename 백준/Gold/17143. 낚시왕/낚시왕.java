fb49225c5a84917849d60fd56d957574ddf8bf25
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-09-17 23:49:57 +0900

    Update 낚시왕.java

--- 백준/Gold/17143. 낚시왕/낚시왕.java
+++ 백준/Gold/17143. 낚시왕/낚시왕.java
@@ -6,6 +6,7 @@ public class Main {
     static List<Integer>[][] map;
 
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
 
