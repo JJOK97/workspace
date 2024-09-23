b87683182e78352a56f0aa37cd08c113ae11a075
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-09-23 20:28:42 +0900

    [Bronze III] Title: 팩토리얼, Time: 180 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/10872. 팩토리얼/팩토리얼.java
@@ -0,0 +1,19 @@
+import java.util.*;
+import java.io.*;
+
+class Main{
+    public static void main(String args[]){
+        Scanner sc = new Scanner(System.in);
+        
+        int N = sc.nextInt();
+        
+        int result = 1;
+        
+        for(int i = 1; i <= N; i++){
+            result *= i;
+        }
+        
+        System.out.println(result);
+        
+    }
+}
