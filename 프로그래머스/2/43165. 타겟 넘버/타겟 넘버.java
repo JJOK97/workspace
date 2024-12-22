5a43285f608429a41e5b16ab667e1b9e90ec0f61
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-12-22 16:37:23 +0900

    [level 2] Title: 타겟 넘버, Time: 237.20 ms, Memory: 155 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -1,3 +1,53 @@
+// import java.util.*;
+
+// class Solution {
+    
+//     static int answer = 0;
+    
+//     public int solution(int[] numbers, int target) {
+        
+//         DFS(numbers, target);
+        
+//         return answer;
+//     }
+    
+//     static class Node{
+//         int i;
+//         int cal;
+        
+//         Node(int i, int cal){
+//             this.i = i;
+//             this.cal = cal;
+//         }
+//     }
+    
+//     static void DFS(int[] numbers, int target){
+    
+//         Stack<Node> stack = new Stack<>();
+//         stack.push(new Node(0,0));
+        
+//         while(!stack.isEmpty()){
+//             Node now = stack.pop();
+            
+//             if(now.i == numbers.length){
+//                 if(now.cal == target){
+//                     answer++;
+//                 }
+//                 continue;
+//             }
+            
+//             int plus = now.cal + numbers[now.i];
+//             int minus = now.cal - numbers[now.i];
+            
+//             stack.push(new Node(now.i + 1, plus));
+//             stack.push(new Node(now.i + 1, minus));
+            
+//         }
+        
+//     }
+    
+// }
+
 import java.util.*;
 
 class Solution {
@@ -5,10 +55,11 @@ class Solution {
     static int answer = 0;
     
     public int solution(int[] numbers, int target) {
-        
-        DFS(numbers, target);
+    
+        bfs(numbers, target);
         
         return answer;
+        
     }
     
     static class Node{
@@ -21,13 +72,14 @@ class Solution {
         }
     }
     
-    static void DFS(int[] numbers, int target){
-    
-        Stack<Node> stack = new Stack<>();
-        stack.push(new Node(0,0));
+    static void bfs(int[] numbers, int target){
         
-        while(!stack.isEmpty()){
-            Node now = stack.pop();
+        Queue<Node> queue = new LinkedList<>();
+        queue.add(new Node(0,0));
+        
+        while(!queue.isEmpty()){
+            
+            Node now = queue.poll();
             
             if(now.i == numbers.length){
                 if(now.cal == target){
@@ -36,14 +88,10 @@ class Solution {
                 continue;
             }
             
-            int plus = now.cal + numbers[now.i];
-            int minus = now.cal - numbers[now.i];
+            queue.add(new Node(now.i + 1, now.cal + numbers[now.i]));
+            queue.add(new Node(now.i + 1, now.cal - numbers[now.i]));
             
-            stack.push(new Node(now.i + 1, plus));
-            stack.push(new Node(now.i + 1, minus));
-            
-        }
-        
+        }        
     }
     
 }
