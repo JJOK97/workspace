f2f2edf0ff18178a5c195348960d8767eaf74d58
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2024-12-22 17:08:25 +0900

    [level 2] Title: 타겟 넘버, Time: 21.87 ms, Memory: 108 MB -BaekjoonHub

--- 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
+++ 프로그래머스/2/43165. 타겟 넘버/타겟 넘버.java
@@ -48,50 +48,83 @@
     
 // }
 
-import java.util.*;
+// import java.util.*;
 
+// class Solution {
+    
+//     static int answer = 0;
+    
+//     public int solution(int[] numbers, int target) {
+    
+//         bfs(numbers, target);
+        
+//         return answer;
+        
+//     }
+    
+//     static class Node{
+//         int i;
+//         int cal;
+        
+//         Node(int i, int cal){
+//             this.i = i;
+//             this.cal = cal;
+//         }
+//     }
+    
+//     static void bfs(int[] numbers, int target){
+        
+//         Queue<Node> queue = new LinkedList<>();
+//         queue.add(new Node(0,0));
+        
+//         while(!queue.isEmpty()){
+            
+//             Node now = queue.poll();
+        
+            
+//             if(now.i == numbers.length){
+//                 if(now.cal == target){
+//                     answer++;
+//                 }
+//                 continue;
+//             }
+            
+//             queue.add(new Node(now.i + 1, now.cal + numbers[now.i]));
+//             queue.add(new Node(now.i + 1, now.cal - numbers[now.i]));
+            
+//         }        
+//     }
+    
+// }
+
+import java.util.*;
 class Solution {
-    
-    static int answer = 0;
-    
+
+    static int result = 0;
+
     public int solution(int[] numbers, int target) {
-    
-        bfs(numbers, target);
-        
-        return answer;
-        
+
+        //0 = 합, 1 = 인덱스
+        calculate(numbers, target, new int[] {0, 0});
+
+        return result;
     }
-    
-    static class Node{
-        int i;
-        int cal;
+
+    public void calculate(int[] numbers, int target, int[] memory){
         
-        Node(int i, int cal){
-            this.i = i;
-            this.cal = cal;
-        }
-    }
-    
-    static void bfs(int[] numbers, int target){
-        
-        Queue<Node> queue = new LinkedList<>();
-        queue.add(new Node(0,0));
-        
-        while(!queue.isEmpty()){
-            
-            Node now = queue.poll();
-            
-            if(now.i == numbers.length){
-                if(now.cal == target){
-                    answer++;
-                }
-                continue;
+        if(memory[1] == numbers.length) {
+            if(memory[0] == target){
+                result++;  
+                return;
             }
-            
-            queue.add(new Node(now.i + 1, now.cal + numbers[now.i]));
-            queue.add(new Node(now.i + 1, now.cal - numbers[now.i]));
-            
-        }        
+            return;
+        }
+
+        // 더하고, 빼기
+        calculate(numbers, target, new int[] {memory[0] + numbers[memory[1]], memory[1]+1});
+        calculate(numbers, target, new int[] {memory[0] - numbers[memory[1]], memory[1]+1});
+
     }
-    
+
+
 }
