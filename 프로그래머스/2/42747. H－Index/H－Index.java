54f4672577aa743ead6ed7a6f056d166e249e1ca
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2025-02-15 22:42:12 +0900

    [level 2] Title: H-Index, Time: 1.62 ms, Memory: 93.6 MB -BaekjoonHub

---
+++ 프로그래머스/2/42747. H－Index/H－Index.java
@@ -0,0 +1,17 @@
+import java.util.*;
+
+class Solution {
+    public int solution(int[] citations) {
+        
+        Arrays.sort(citations);
+        int n = citations.length;
+
+        for (int i = 0; i < n; i++) {
+            if (citations[i] >= n - i) {
+                return n - i;
+            }
+        }
+        
+        return 0;
+    }
+}
