629ddf8a29c30c537e938066a1259a373b8889ad
Jinseok_Ock <133585698+JJOK97@users.noreply.github.com>
2025-02-21 10:02:57 +0900

    [level 3] Title: 여행경로, Time: 0.54 ms, Memory: 84.6 MB -BaekjoonHub

--- 프로그래머스/3/43164. 여행경로/README.md
+++ 프로그래머스/3/43164. 여행경로/README.md
@@ -1,10 +1,10 @@
 # [level 3] 여행경로 - 43164 
 
-[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164?language=java) 
+[문제 링크](https://school.programmers.co.kr/learn/courses/30/lessons/43164) 
 
 ### 성능 요약
 
-메모리: 96.9 MB, 시간: 0.63 ms
+메모리: 84.6 MB, 시간: 0.54 ms
 
 ### 구분
 
@@ -16,7 +16,7 @@
 
 ### 제출 일자
 
-2025년 02월 20일 15:00:00
+2025년 02월 21일 10:02:56
 
 ### 문제 설명
 
--- 프로그래머스/3/43164. 여행경로/여행경로.java
+++ 프로그래머스/3/43164. 여행경로/여행경로.java
@@ -1,35 +1,34 @@
 import java.util.*;
 
 class Solution {
-    
-    static Map<String, PriorityQueue<String>> graph = new HashMap<>();
-    static List<String> route = new ArrayList<>();
+    static Map<String, PriorityQueue<String>> map = new HashMap<>();
+    static List<String> result = new ArrayList<>();
     
     public String[] solution(String[][] tickets) {
-       
+        
         for(String[] ticket : tickets){
             
-            graph.putIfAbsent(ticket[0], new PriorityQueue<>());
-            graph.get(ticket[0]).offer(ticket[1]);
+            map.putIfAbsent(ticket[0], new PriorityQueue<>());
+            map.get(ticket[0]).offer(ticket[1]);
             
         }
         
         dfs("ICN");
         
-        Collections.reverse(route);
-        return route.toArray(new String[route.size()]);
-        
+        Collections.reverse(result);
+        return result.toArray(new String[result.size()]);
     }
     
     public static void dfs(String airport){
         
-        PriorityQueue<String> pq = graph.get(airport);
-        while (pq != null && !pq.isEmpty()){
-            dfs(pq.poll());
+        PriorityQueue<String> pq = map.get(airport);
+        
+        while(pq != null && !pq.isEmpty()){
+            String s = pq.poll();
+            dfs(s);
         }
         
-        route.add(airport);
+        result.add(airport);
         
     }
-
 }
